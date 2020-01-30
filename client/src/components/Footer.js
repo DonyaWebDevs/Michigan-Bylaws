709299677f540ec4e85c3920448b834e5ec5ae08
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-01-30 12:43:33 -0700

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,25 @@ const Footer = () => {
     return (
         <div>
             <footer>
-                Footer
+                <div>
+                    <img></img>
+                    <img></img>
+                    <div>
+                        <p>KVEVIN CERMAK REAL ESTATE GROUP</p>
+                    </div>
+                    <div>
+                        <p>Northville/ Birmingham/ Livonia</p>
+                        <p>116 W. Main Street</p>
+                        <p>Northville, MI 48167</p>
+                    </div>
+                    <div>
+                        <p>(248)658-8445</p>
+                    </div>
+                        <p>{&copy} All Rights Reserved</p>
+                </div>
+                <div>
+
+                </div>
             </footer>
         </div>
     )
