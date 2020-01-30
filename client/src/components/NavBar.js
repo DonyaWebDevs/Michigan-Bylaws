4610eec208863457c1abfbef2369000e5cf0dd37
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-01-29 23:21:20 -0700

    navbar and footer scafolded

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const NavBar = () => {
+
+
+    return(
+        <div>
+            <header>
+                NavBar
+            </header>
+        </div>
+    )
+}
+
+export default NavBar;
