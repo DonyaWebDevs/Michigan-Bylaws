0b0e4613ef150896a7780e3a5d3473605a6eeab3
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-05 22:23:17 -0500

    update

--- client/src/components/ToggelButton/ToggleButton.js
+++ client/src/components/ToggelButton/ToggleButton.js
@@ -8,6 +8,7 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Button from "@material-ui/core/Button"
 
 const ToggleButton = props => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -26,27 +27,30 @@ const ToggleButton = props => {
 
     return(
 
-        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-        <DropdownToggle color="light" caret>
-            <FontAwesomeIcon icon={faBars} size="2x"/>
-        </DropdownToggle>
-        <DropdownMenu right>
+      //   <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      //   <DropdownToggle color="light" caret>
+      //       <FontAwesomeIcon icon={faBars} size="2x"/>
+      //   </DropdownToggle>
+      //   <DropdownMenu right>
          
           
-          <DropdownItem><a href="/">Item </a></DropdownItem>
-          <DropdownItem><a href="/">Home</a></DropdownItem>
-         <DropdownItem><a href="/">Advanced Search</a></DropdownItem>
-         <DropdownItem><a href="/">Market Reports</a></DropdownItem>
-         <DropdownItem><a href="/">Buying</a></DropdownItem>
-         <DropdownItem><a href="/">Selling</a></DropdownItem>
-         <DropdownItem><a href="/">Foreclosures</a></DropdownItem>
-         <DropdownItem><a href="/">About</a></DropdownItem>
-         <DropdownItem><a href="/">Contact Us</a></DropdownItem>
-         <DropdownItem><a href="/">By Laws</a></DropdownItem>
+      //     <DropdownItem><a href="/">Item </a></DropdownItem>
+      //     <DropdownItem><a href="/">Home</a></DropdownItem>
+      //    <DropdownItem><a href="/">Advanced Search</a></DropdownItem>
+      //    <DropdownItem><a href="/">Market Reports</a></DropdownItem>
+      //    <DropdownItem><a href="/">Buying</a></DropdownItem>
+      //    <DropdownItem><a href="/">Selling</a></DropdownItem>
+      //    <DropdownItem><a href="/">Foreclosures</a></DropdownItem>
+      //    <DropdownItem><a href="/">About</a></DropdownItem>
+      //    <DropdownItem><a href="/">Contact Us</a></DropdownItem>
+      //    <DropdownItem><a href="/">By Laws</a></DropdownItem>
           
-        </DropdownMenu>
-      </Dropdown>
+      //   </DropdownMenu>
+      // </Dropdown>
        
+    <Button>
+      Test
+    </Button>
     )
 }
 
